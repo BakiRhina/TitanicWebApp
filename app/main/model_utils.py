d699dd627a1efe833716092bea01e7ed5e5c0d21
8kyu <guillemsenabre@gmail.com>
2024-02-17 19:29:07 +0100

    feat: prediction method in model class created

--- app/main/model_utils.py
+++ app/main/model_utils.py
@@ -6,10 +6,7 @@ class Model():
   def __init__(self, model_path):
     self.model = self._load_model(model_path)
 
-  def _load_model(self, model_path):
-    with open(model_path, 'rb') as file:
-      model = joblib.load(file)
-    return model
+
   
   def process_data(self, data_list):
     # 1. data_list comes as: [sex, fare, cabin]
@@ -23,6 +20,16 @@ class Model():
     pclass = self._get_pclass(data[1])
     data_def = [passengerId, pclass, data[0], data[1], data[2]]
     return np.array([data_def])
+  
+  def get_prediction(self, ndarray_values):
+    prediction = self.model.predict(ndarray_values)
+    
+    return prediction
+
+  def _load_model(self, model_path):
+    with open(model_path, 'rb') as file:
+      model = joblib.load(file)
+    return model
     
   def _get_pclass(self, fare):
     if fare < 70:
